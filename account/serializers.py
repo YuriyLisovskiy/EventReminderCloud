b42d3facb82b5ebe85c85273df6be0035135d7de
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-02-16 13:45:12 +0200

    Fixed auth model, removed redundant serializer for editing and provided its logic in AccountSerializer

--- account/serializers.py
+++ account/serializers.py
@@ -7,57 +7,38 @@ from rest_framework import serializers
 
 class AccountSerializer(serializers.ModelSerializer):
 
-	username = serializers.CharField(required=True, allow_blank=False, allow_null=False)
-	email = serializers.EmailField(required=True, allow_blank=False)
-
 	def validate(self, data):
-		Account.remove(data.get('username'))
-
-		if Account.objects.filter(email=data.get('email')).exists():
-			raise serializers.ValidationError('user already exists')
-		if Account.objects.filter(username=data.get('username')).exists():
-			raise serializers.ValidationError('user already exists')
+		if self.instance is None:
+			if Account.objects.filter(email=data.get('email')).exists():
+				raise serializers.ValidationError('user already exists')
+			if Account.objects.filter(username=data.get('username')).exists():
+				raise serializers.ValidationError('user already exists')
 		return data
 
 	def create(self, validated_data):
-
-		random_password = self.rand_password()
-		validated_data['password'] = random_password
-
-		print(validated_data)
-
+		password = _rand_password()
+		validated_data['password'] = password
 		account = Account.create(**validated_data)
+		account.save()
 
-		print('\nUSERNAME: {}\nPASSWORD: {}\n\n'.format(account.username, random_password))
-
-		print(Account.get_by_id(account.username).email)
+		print('\nUSERNAME: {}\nPASSWORD: {}\n\n'.format(account.username, password))
 
 		return account
 
-	@staticmethod
-	def rand_password():
-		chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-		password = random.choice(chars)
-		password_len = random.randrange(8, 13)
-		for _ in range(password_len - 1):
-			password += random.choice(chars)
-		return password
-
-	class Meta:
-		model = Account
-		fields = ('username', 'email')
-
-
-class AccountEditSerializer(serializers.ModelSerializer):
-
-	pk = serializers.IntegerField(read_only=True)
-	password = serializers.CharField(required=True, allow_blank=False, allow_null=False)
-
 	def update(self, instance, validated_data):
-		instance.set_password(validated_data.get('password', instance.password))
+		instance = instance.edit(password=validated_data.get('password', None))
 		instance.save()
 		return instance
 
 	class Meta:
 		model = Account
-		fields = ('email',)
+		fields = ('username', 'email', 'password')
+
+
+def _rand_password():
+	chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+	password = random.choice(chars)
+	password_len = random.randrange(8, 13)
+	for _ in range(password_len - 1):
+		password += random.choice(chars)
+	return password
--- account/views.py
+++ account/views.py
@@ -7,43 +7,35 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
 
-from account.serializers import (
-	AccountSerializer,
-	AccountEditSerializer
-)
+from account.serializers import AccountSerializer
 from account.models import Account
 
 
-class AccountListView(CreateAPIView):
+class AccountCreateAPIView(CreateAPIView):
 	serializer_class = AccountSerializer
 
+	# credentials_sender = lambda username, password: {'username': username, 'password': password}
 
-class AccountDetailsView(APIView):
+	# def create(self, request, *args, **kwargs):
+	# 	serializer = self.serializer_class
+
+	# 	print(serializer.data)
+
+	# 	data = serializer.data
+
+
+class AccountEditAPIView(APIView):
 	authentication_classes = (authentication.TokenAuthentication,)
 	permission_classes = (permissions.IsAuthenticated,)
 
 	@staticmethod
-	def get(request):
-		pk = request.GET.get('pk', None)
-		if pk is None:
-			return Response({'error': 'primary key is not provided'}, status=status.HTTP_400_BAD_REQUEST)
-		account = Account.get_by_id(pk)
-		if account is None:
-			return Response(status=status.HTTP_404_NOT_FOUND)
-		if account.username != request.user.username or account.email != request.user.email:
-			return Response(status=status.HTTP_403_FORBIDDEN)
-		serializer = AccountSerializer(account)
-		return Response(serializer.data, status=status.HTTP_200_OK)
-
-	@staticmethod
 	def post(request):
-		pk = request.POST.get('pk', None)
-		if pk is None:
-			return Response({'error': 'primary key is not provided'}, status=status.HTTP_400_BAD_REQUEST)
-		account = Account.get_by_id(pk)
+		account = Account.get_by_pk(request.user.username)
 		if account is None:
 			return Response(status=status.HTTP_404_NOT_FOUND)
-		serializer = AccountEditSerializer(instance=account, data=request.data)
+		data = request.data.copy()
+		data['username'] = request.user.username
+		serializer = AccountSerializer(instance=account, data=data)
 		if serializer.is_valid():
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_200_OK)
